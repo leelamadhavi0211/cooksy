0d36acc30f7627ef2f308f13e9a0799f0e0894dc
Myloji Leelamandhavi <mailojileelamadhavi246@gmail.com>
2025-10-18 02:42:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,15 @@ import express from "express";
 import cors from "cors";
 import axios from "axios";
 import admin from "firebase-admin";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-// Initialize Firebase using environment variables
+import dotenv from "dotenv";
+
+dotenv.config(); // load .env variables
+
 const serviceAccount = {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 };
 
 admin.initializeApp({
